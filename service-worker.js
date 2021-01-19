169fb53ccf0d50099fbc99014332009ac362e929
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-19 12:00:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.964d846073e112a512b99cf5f7d9e614.js"
 );
 
 self.addEventListener('message', (event) => {
